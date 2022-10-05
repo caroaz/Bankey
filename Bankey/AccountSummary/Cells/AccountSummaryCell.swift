cbded86729643876417fba196ffe78a3492d7299
caroaz <carolinaazcurra8@gmail.com>
2022-10-05 10:24:24 -0300

    add currencyFormatter

--- Bankey/AccountSummary/Cells/AccountSummaryCell.swift
+++ Bankey/AccountSummary/Cells/AccountSummaryCell.swift
@@ -19,6 +19,10 @@ class AccountSummaryCell : UITableViewCell {
     struct ViewModel {
         let accountType: AccountType
         let accountName: String
+        let balance: Decimal
+        var balanceAsAttributedString: NSAttributedString {
+               return CurrencyFormatter().makeAttributedCurrency(balance)
+           }
     }
     
     let viewModel: ViewModel? = nil
@@ -133,6 +137,8 @@ extension AccountSummaryCell {
     func configure(with vm: ViewModel) {
         typeLabel.text = vm.accountType.rawValue
         nameLabel.text = vm.accountName
+        balanceAmountLabel.attributedText = vm.balanceAsAttributedString
+        
         switch vm.accountType {
         case .Banking:
             underlineView.backgroundColor = .systemTeal
